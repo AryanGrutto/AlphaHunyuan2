d4aa5368087d00f098bf662f840ee675ded581e0
Aryan Behzadi <aryan@grutto.com>
2025-08-30 14:05:26 +0200

    adjusted some stuff

--- hy3dgen/texgen/hunyuanpaint/__init__.py
+++ hy3dgen/texgen/hunyuanpaint/__init__.py
@@ -11,3 +11,6 @@
 # optimizer states), machine-learning model code, inference-enabling code, training-enabling code,
 # fine-tuning enabling code and other elements of the foregoing made publicly available
 # by Tencent in accordance with TENCENT HUNYUAN COMMUNITY LICENSE AGREEMENT.
+
+# Package imports
+from .pipeline import *
